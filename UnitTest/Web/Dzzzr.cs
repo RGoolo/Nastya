8f7cdf0da36d3d46eda628ea22a1bb1df22ae6d1
Roman Gulo <gulord@yandex.ru>
2019-03-24 22:29:20 +0300

    Add test

---
+++ UnitTest/Web/Dzzzr.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Model.Types.Enums;
+using Model.Types.Class;
+using Web.DZR;
+
+namespace UnitTest.Model
+{
+	public class CheckRecognizeCode
+	{
+		
+		[Theory]
+		[MemberData(nameof(TypeUsers))]
+		public void CoordTest(string text, string prefix, string[] result = null)
+		{
+			var codes = Dzr.GetCodes(text, prefix) ?? new List<string>();
+
+			if (codes == null && result == null)
+				return;
+
+			for(int i = 0; i < codes.Count; ++i)
+				Assert.Equal(codes[i], result[i]);
+		}
+
+		public static IEnumerable<object[]> TypeUsers()
+		{
+			yield return new object[] { "abvd", "" };
+			yield return new object[] { " d1", ""};
+			yield return new object[] { "1", "1d", new[] { "1dr1", "1d1r" } };
+			yield return new object[] { "1a", "1d"};
+			yield return new object[] { "1@", "1d", new[] { "1d1r" } };
+			yield return new object[] { "r", "1d", new[] { "1dr" } };
+			yield return new object[] { "pдd", "1d", new[] { "1drdd" } };
+			yield return new object[] { "1)", "1d", new[] { "1d1r" } };
+		}
+    }
+}
---
+++ UnitTest/Web/RegEx.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Xunit;
+using Model.Types.Enums;
+using Model.Types.Class;
+using System.Text.RegularExpressions;
+
+namespace UnitTest
+{
+	public class RegexTest
+	{
+
+		public static string pattern = "(</[^abi][^>]*>)|(<[^abi/][^>]*>)|(</(\\w){2,}>)|(<a[^ ][^>]*>)|(<(b|i)[^>]+>)"; //(<[^>a/]*>)|(</[^a>]*>)|(</[^a][^ ][^>]*>)
+
+		[Theory]
+		[MemberData(nameof(TypeUsers))]
+		public void CoordTest(string input, string output)
+		{
+			//var 
+			var str = new  Regex(pattern).Replace(input, string.Empty);
+			Assert.Equal(str, output);
+
+		}
+
+		public static IEnumerable<object[]> TypeUsers()
+		{
+			yield return new object[] { "<b>bold</b>", "<b>bold</b>" };
+			yield return new object[] { "<ba>bold</ba>", "bold" };
+
+			yield return new object[] { "<ab>cd</ab>", "cd" };
+			yield return new object[] { "<a href=\"link\">namelink</a>", "<a href=\"link\">namelink</a>" };
+		
+
+			yield return new object[] { "<i>italic</i>", "<i>italic</i>" };
+			yield return new object[] { "<in>tI</in>", "tI" };
+
+			yield return new object[] { "<h>col</h>", "col" };
+			yield return new object[] { "<head>ahead</head>", "ahead" };
+		}
+	}
+}
